47b4f4b302c49fd258540bf4c97e85844e47cd7a
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-03-04 10:35:54 +0600

    update 04/03/23

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,67 @@
 
        <link  rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
-
     <title>agriculture</title>
   </head>
   <body>
-    <h1 class="header-text">Hello, world!</h1>
+    <h1 class="header-text"> </h1>
 
+ <section>
+  <div class="container ">
 
+  <section class="row">
+    <div class="col-lg-9 col-md-9 col-sm-12 col-12">
+        <h6>ড্যাশবোর্ড : কৃষি সম্প্রাসারণ অধিদপ্তর,  সারেজমিন উইং</h6>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-12">
+        <h6><strong>হুমায়ূন কবির</strong></h6>
+        <p class="nav-designation">অতিরিক্ত উপপরিচালক (কন্ট্রোল রুম),<br/>
+কৃষি সম্প্রসারণ অধিদপ্তর</p>
+    </div>
+  </section>
+
+  <section>
+    <h6>একনজরে</h6>
+    <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+         <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>জেলা</option>
+            <option value="1">ঢাকা</option>
+            <option value="2">চাঁপাই-নবাবগঞ্জ</option>
+            <option value="3">নাটর</option>
+          </select>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+         <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>উপজেলা</option>
+            <option value="1">শান্তিগঞ্জ-উপজেলা</option>
+            <option value="2">দাসার-উপজেলা</option>
+            <option value="3">শিবপগঞ্জ-উপজেলা</option>
+          </select>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+         <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-6 col-6">
+         <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+      </div>
+    </div>
+  </section>
+
+  </div>
+    
+    </div>
+ </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
